c0d75744e61fffbf48d267e3cb0cfb75bdea091b
Giorgi Garsevanishvili <george.garsevanidi@gmail.com>
2024-04-14 20:57:31 +0300

    V2-DF-F

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -1,6 +1,14 @@
-
 let currentPlayer = 'X';
 let cells = ['', '', '', '', '', '', '', '', ''];
+let result = JSON.parse(localStorage.getItem('score')) || {
+  X: 0,
+  O: 0,
+  tie: 0
+}
+
+resultColumn();
+
+displayFlow();
 
 document.querySelector('.btn1').addEventListener('click', () => {
   cellClicked(0);
@@ -42,15 +50,8 @@ function cellClicked(index){
   if (cells[index] === ''){
       cells[index] = currentPlayer;
       renderBoard();
-      if (checkWin()){
-        alert(currentPlayer + 'wins')
-        resetGame();
-      } else if (checkDraw()){
-        alert('Draw')
-        resetGame();
-      } else {
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-            }
+      displayResult();
+      
   }
 }
 
@@ -97,3 +98,87 @@ function checkWin() {
   }
   return false;
   }
+
+
+
+function displayFlow (){
+  let Player = currentPlayer;
+  document.querySelector('.gameFlow').innerHTML = `YOUR TURN ${Player}`;
+}
+
+function displayResult (){
+  if (checkWin()){
+    value = true;
+    resultClass(value);
+  } else if (checkDraw()){
+    value = false;
+    resultClass(value);
+  } else {
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    displayFlow();
+        }
+
+  document.querySelector('.closeButton').addEventListener('click', () => {
+    closeButton();
+  })
+
+  function resultClass (value){
+    document.querySelector('.status-box').classList.add('winStatus-box');
+    document.querySelector('.status-cont').classList.add('winStatus-cont');
+
+    if (value === true ){
+      document.querySelector('.winStatus-cont').innerHTML =  `
+      <div class="res-box">
+        <div class="close">
+          <button class="closeButton">X</button>
+        </div>
+        <div class="res-cont">${currentPlayer}, You Win!</div>
+      </div>
+    `
+      if (currentPlayer === 'X'){
+        result.X ++;
+        resultColumn();
+      } else if (currentPlayer === 'O'){
+        result.O ++;
+        resultColumn();
+      } 
+    } else if (value === false) {
+      result.tie ++;
+      resultColumn();
+      document.querySelector('.winStatus-cont').innerHTML =  `
+      <div class="res-box">
+        <div class="close">
+          <button class="closeButton">X</button>
+        </div>
+        <div class="res-cont">Draw!</div>
+      </div>
+    `
+    }
+    saveResult();
+    
+  }
+  
+  function  closeButton (){
+    document.querySelector('.winStatus-cont').innerHTML ='';
+    document.querySelector('.status-box').classList.remove('winStatus-box')
+    document.querySelector('.status-cont').classList.remove('winStatus-cont')
+    resetGame();
+  }
+}
+
+function resultColumn (){
+  document.querySelector('.result').innerHTML = `X = ${result.X}  O = ${result.O} Draw = ${result.tie}`;
+}
+
+document.querySelector('.reset-button').addEventListener('click', () => {
+  result.O = 0;
+  result.X = 0;
+  result.tie = 0;
+  resultColumn();
+  resetGame();
+  saveResult();
+})
+
+function saveResult (){
+  localStorage.setItem('score', JSON.stringify(result));
+}
